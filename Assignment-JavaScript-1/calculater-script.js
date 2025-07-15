28a889eb6dfe627d3c25eacbd50f00f894fe9e4f
Praphan <77480427+kaopancha@users.noreply.github.com>
2025-07-15 23:41:39 +0700

    reset after equals

--- Assignment-JavaScript-1/calculater-script.js
+++ Assignment-JavaScript-1/calculater-script.js
@@ -24,6 +24,16 @@ function handleInput(value) {
   let err = 0;
   // ตัวเลข กับ .
   if (!isNaN(value) || (value === '.' && !currentInput.includes('.'))) {
+    if (isEqual) {
+      // start a new calculation after finishing the previous one
+      firstOperand = '';
+      operator = '';
+      secondOperand = '';
+      historyText = '';
+      currentInput = '';
+      isEqual = false;
+    }
+
     if (currentInput.replace('.', '').length < 14)
       currentInput += value;
     isChangeOperator = false;
